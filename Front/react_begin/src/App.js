3be3d3aef9ed74ed1566ecc15426b4f81f3260a4
윤민지 <gee2450@kookmin.ac.kr>
2021-08-26 18:19:41 +0900

    Add Input useState로 input value 관리

--- Front/react_begin/src/App.js
+++ Front/react_begin/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
     <Wrapper>
       <Hello name="react" color="red" isSpecial />
       <Hello color="pink"/>
-      <Counter />
+      <Counter /><br/>
+      <InputSample />
     </Wrapper>
   );
 }
