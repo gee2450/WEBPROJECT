ac7a868dd21260fa1517a9933e4244676addafa1
윤민지 <gee2450@kookmin.ac.kr>
2021-10-11 00:23:21 +0900

    immer library 이용해보기

--- Front/react_begin/src/App.js
+++ Front/react_begin/src/App.js
@@ -5,6 +5,7 @@ import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -41,22 +42,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     default:
       return state;
   }
